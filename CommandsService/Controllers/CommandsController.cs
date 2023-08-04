a8b97b69aec06026131ec78860cafeb551569782
Sigalini, Danilo <danilo.sigalini@ext.europassistance.it>
2023-08-04 07:58:02 +0200

    add get default method to Commands

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ILogger<CommandsController> _logger;
+
+        public CommandsController(ILogger<CommandsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            _logger.LogInformation("Called Get Method");
+            return Ok("OK");
+        }
+    }
+}
